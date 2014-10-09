2016f7f1d3e9aab80ef71685f0737b89e8380da2
Ken Ohata <ohata@daraf.co.jp>
2014-10-09 20:56:14 +0900

    update words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,7 @@
 class WordsController < ApplicationController
   def show
+    id    = CGI.escape(params[:id])
+    @file = open("http://dic.nicovideo.jp/a/#{id}")
+    @word = NicoDict::Word.new(@file)
   end
 end
